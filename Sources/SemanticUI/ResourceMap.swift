6b02d91e66e69df51e1e1f92b92b2175b2b3a0d1
Helge Heß <devteam@zeezide.de>
2018-05-29 13:23:03 +0200

    Add conversion of SemanticUI 2.3.1

---
+++ Sources/SemanticUI/ResourceMap.swift
@@ -0,0 +1,378 @@
+// Generated on Tue May 29 13:22:31 CEST 2018
+//
+
+import struct Foundation.Data
+
+public extension SemanticUI {
+
+  /**
+   * Lookup a SemanticUI resource. The returned Data
+   * contains the gzip compressed resource.
+   *
+   * Available resource names:
+   *
+   * - "semantic.min.css"
+   * - "semantic.min.js"
+   * - "components/accordion.min.css"
+   * - "components/ad.min.css"
+   * - "components/breadcrumb.min.css"
+   * - "components/button.min.css"
+   * - "components/card.min.css"
+   * - "components/checkbox.min.css"
+   * - "components/comment.min.css"
+   * - "components/container.min.css"
+   * - "components/dimmer.min.css"
+   * - "components/divider.min.css"
+   * - "components/dropdown.min.css"
+   * - "components/embed.min.css"
+   * - "components/feed.min.css"
+   * - "components/flag.min.css"
+   * - "components/form.min.css"
+   * - "components/grid.min.css"
+   * - "components/header.min.css"
+   * - "components/icon.min.css"
+   * - "components/image.min.css"
+   * - "components/input.min.css"
+   * - "components/item.min.css"
+   * - "components/label.min.css"
+   * - "components/list.min.css"
+   * - "components/loader.min.css"
+   * - "components/menu.min.css"
+   * - "components/message.min.css"
+   * - "components/modal.min.css"
+   * - "components/nag.min.css"
+   * - "components/popup.min.css"
+   * - "components/progress.min.css"
+   * - "components/rail.min.css"
+   * - "components/rating.min.css"
+   * - "components/reset.min.css"
+   * - "components/reveal.min.css"
+   * - "components/search.min.css"
+   * - "components/segment.min.css"
+   * - "components/shape.min.css"
+   * - "components/sidebar.min.css"
+   * - "components/site.min.css"
+   * - "components/statistic.min.css"
+   * - "components/step.min.css"
+   * - "components/sticky.min.css"
+   * - "components/tab.min.css"
+   * - "components/table.min.css"
+   * - "components/transition.min.css"
+   * - "components/video.min.css"
+   * - "components/accordion.min.js"
+   * - "components/api.min.js"
+   * - "components/checkbox.min.js"
+   * - "components/colorize.min.js"
+   * - "components/dimmer.min.js"
+   * - "components/dropdown.min.js"
+   * - "components/embed.min.js"
+   * - "components/form.min.js"
+   * - "components/modal.min.js"
+   * - "components/nag.min.js"
+   * - "components/popup.min.js"
+   * - "components/progress.min.js"
+   * - "components/rating.min.js"
+   * - "components/search.min.js"
+   * - "components/shape.min.js"
+   * - "components/sidebar.min.js"
+   * - "components/site.min.js"
+   * - "components/state.min.js"
+   * - "components/sticky.min.js"
+   * - "components/tab.min.js"
+   * - "components/transition.min.js"
+   * - "components/video.min.js"
+   * - "components/visibility.min.js"
+   * - "components/visit.min.js"
+   * - "themes/default/assets/fonts/brand-icons.eot"
+   * - "themes/default/assets/fonts/icons.eot"
+   * - "themes/default/assets/fonts/outline-icons.eot"
+   * - "themes/default/assets/fonts/brand-icons.svg"
+   * - "themes/default/assets/fonts/icons.svg"
+   * - "themes/default/assets/fonts/outline-icons.svg"
+   * - "themes/default/assets/fonts/brand-icons.ttf"
+   * - "themes/default/assets/fonts/icons.ttf"
+   * - "themes/default/assets/fonts/outline-icons.ttf"
+   * - "themes/default/assets/fonts/icons.otf"
+   * - "themes/default/assets/fonts/brand-icons.woff"
+   * - "themes/default/assets/fonts/icons.woff"
+   * - "themes/default/assets/fonts/outline-icons.woff"
+   * - "themes/default/assets/fonts/brand-icons.woff2"
+   * - "themes/default/assets/fonts/icons.woff2"
+   * - "themes/default/assets/fonts/outline-icons.woff2"
+   * - "themes/default/assets/images/flags.png"
+   *
+   */
+  public static func resourceNamed(_ name: String) -> Data? {
+    switch name {
+
+      case "semantic.min.css":
+        return data_semantic_min_css
+
+      case "semantic.min.js":
+        return data_semantic_min_js
+
+      case "components/accordion.min.css":
+        return data_accordion_min_css
+
+      case "components/ad.min.css":
+        return data_ad_min_css
+
+      case "components/breadcrumb.min.css":
+        return data_breadcrumb_min_css
+
+      case "components/button.min.css":
+        return data_button_min_css
+
+      case "components/card.min.css":
+        return data_card_min_css
+
+      case "components/checkbox.min.css":
+        return data_checkbox_min_css
+
+      case "components/comment.min.css":
+        return data_comment_min_css
+
+      case "components/container.min.css":
+        return data_container_min_css
+
+      case "components/dimmer.min.css":
+        return data_dimmer_min_css
+
+      case "components/divider.min.css":
+        return data_divider_min_css
+
+      case "components/dropdown.min.css":
+        return data_dropdown_min_css
+
+      case "components/embed.min.css":
+        return data_embed_min_css
+
+      case "components/feed.min.css":
+        return data_feed_min_css
+
+      case "components/flag.min.css":
+        return data_flag_min_css
+
+      case "components/form.min.css":
+        return data_form_min_css
+
+      case "components/grid.min.css":
+        return data_grid_min_css
+
+      case "components/header.min.css":
+        return data_header_min_css
+
+      case "components/icon.min.css":
+        return data_icon_min_css
+
+      case "components/image.min.css":
+        return data_image_min_css
+
+      case "components/input.min.css":
+        return data_input_min_css
+
+      case "components/item.min.css":
+        return data_item_min_css
+
+      case "components/label.min.css":
+        return data_label_min_css
+
+      case "components/list.min.css":
+        return data_list_min_css
+
+      case "components/loader.min.css":
+        return data_loader_min_css
+
+      case "components/menu.min.css":
+        return data_menu_min_css
+
+      case "components/message.min.css":
+        return data_message_min_css
+
+      case "components/modal.min.css":
+        return data_modal_min_css
+
+      case "components/nag.min.css":
+        return data_nag_min_css
+
+      case "components/popup.min.css":
+        return data_popup_min_css
+
+      case "components/progress.min.css":
+        return data_progress_min_css
+
+      case "components/rail.min.css":
+        return data_rail_min_css
+
+      case "components/rating.min.css":
+        return data_rating_min_css
+
+      case "components/reset.min.css":
+        return data_reset_min_css
+
+      case "components/reveal.min.css":
+        return data_reveal_min_css
+
+      case "components/search.min.css":
+        return data_search_min_css
+
+      case "components/segment.min.css":
+        return data_segment_min_css
+
+      case "components/shape.min.css":
+        return data_shape_min_css
+
+      case "components/sidebar.min.css":
+        return data_sidebar_min_css
+
+      case "components/site.min.css":
+        return data_site_min_css
+
+      case "components/statistic.min.css":
+        return data_statistic_min_css
+
+      case "components/step.min.css":
+        return data_step_min_css
+
+      case "components/sticky.min.css":
+        return data_sticky_min_css
+
+      case "components/tab.min.css":
+        return data_tab_min_css
+
+      case "components/table.min.css":
+        return data_table_min_css
+
+      case "components/transition.min.css":
+        return data_transition_min_css
+
+      case "components/video.min.css":
+        return data_video_min_css
+
+      case "components/accordion.min.js":
+        return data_accordion_min_js
+
+      case "components/api.min.js":
+        return data_api_min_js
+
+      case "components/checkbox.min.js":
+        return data_checkbox_min_js
+
+      case "components/colorize.min.js":
+        return data_colorize_min_js
+
+      case "components/dimmer.min.js":
+        return data_dimmer_min_js
+
+      case "components/dropdown.min.js":
+        return data_dropdown_min_js
+
+      case "components/embed.min.js":
+        return data_embed_min_js
+
+      case "components/form.min.js":
+        return data_form_min_js
+
+      case "components/modal.min.js":
+        return data_modal_min_js
+
+      case "components/nag.min.js":
+        return data_nag_min_js
+
+      case "components/popup.min.js":
+        return data_popup_min_js
+
+      case "components/progress.min.js":
+        return data_progress_min_js
+
+      case "components/rating.min.js":
+        return data_rating_min_js
+
+      case "components/search.min.js":
+        return data_search_min_js
+
+      case "components/shape.min.js":
+        return data_shape_min_js
+
+      case "components/sidebar.min.js":
+        return data_sidebar_min_js
+
+      case "components/site.min.js":
+        return data_site_min_js
+
+      case "components/state.min.js":
+        return data_state_min_js
+
+      case "components/sticky.min.js":
+        return data_sticky_min_js
+
+      case "components/tab.min.js":
+        return data_tab_min_js
+
+      case "components/transition.min.js":
+        return data_transition_min_js
+
+      case "components/video.min.js":
+        return data_video_min_js
+
+      case "components/visibility.min.js":
+        return data_visibility_min_js
+
+      case "components/visit.min.js":
+        return data_visit_min_js
+
+      case "themes/default/assets/fonts/brand-icons.eot":
+        return data_brand_icons_eot
+
+      case "themes/default/assets/fonts/icons.eot":
+        return data_icons_eot
+
+      case "themes/default/assets/fonts/outline-icons.eot":
+        return data_outline_icons_eot
+
+      case "themes/default/assets/fonts/brand-icons.svg":
+        return data_brand_icons_svg
+
+      case "themes/default/assets/fonts/icons.svg":
+        return data_icons_svg
+
+      case "themes/default/assets/fonts/outline-icons.svg":
+        return data_outline_icons_svg
+
+      case "themes/default/assets/fonts/brand-icons.ttf":
+        return data_brand_icons_ttf
+
+      case "themes/default/assets/fonts/icons.ttf":
+        return data_icons_ttf
+
+      case "themes/default/assets/fonts/outline-icons.ttf":
+        return data_outline_icons_ttf
+
+      case "themes/default/assets/fonts/icons.otf":
+        return data_icons_otf
+
+      case "themes/default/assets/fonts/brand-icons.woff":
+        return data_brand_icons_woff
+
+      case "themes/default/assets/fonts/icons.woff":
+        return data_icons_woff
+
+      case "themes/default/assets/fonts/outline-icons.woff":
+        return data_outline_icons_woff
+
+      case "themes/default/assets/fonts/brand-icons.woff2":
+        return data_brand_icons_woff2
+
+      case "themes/default/assets/fonts/icons.woff2":
+        return data_icons_woff2
+
+      case "themes/default/assets/fonts/outline-icons.woff2":
+        return data_outline_icons_woff2
+
+      case "themes/default/assets/images/flags.png":
+        return data_flags_png
+
+      default: return nil
+    }
+  }
+}
